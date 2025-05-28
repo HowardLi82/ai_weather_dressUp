3165e497748e29977f2825dcb71b1eea55c75f8d
YaShaneLi <zxc48669@gmail.com>
2025-05-28 16:48:48 +0800

    修改vite.config裡的port設定

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    strictPort: true
-  },
+  // server: {
+  //   port: 5173,
+  //   strictPort: true
+  // },
   base: "/weathercon/", // 🔥 設定根路徑
 
 })
