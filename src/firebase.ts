aab7a9133c2fb179cd1b06d07174e9d4c51b1407
YaShaneLi <zxc48669@gmail.com>
2025-05-28 15:08:07 +0800

    修正modules

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 import { config } from "./config";
 import { getFirestore } from "firebase/firestore";
 
@@ -8,4 +8,4 @@ import { getFirestore } from "firebase/firestore";
 // Initialize Firebase
 const app = initializeApp(config.firebase);
 export const db = getFirestore(app);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
